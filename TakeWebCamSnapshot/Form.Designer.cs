f5b8c17f291e75ec74659805279e92cfbac52a74
Semyon Labzov <s-labzov@armo.ru>
2023-02-14 09:59:46 +0300

    - refactoring

--- TakeWebCamSnapshot/Form.Designer.cs
+++ TakeWebCamSnapshot/Form.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TakeWebCamSnapshot
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+            this.videoView = new LibVLCSharp.WinForms.VideoView();
             this.button1 = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            this.textBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView)).BeginInit();
             this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -41,43 +42,42 @@
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // videoView1
+            // videoView
             // 
-            this.videoView1.BackColor = System.Drawing.Color.Black;
-            this.videoView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.videoView1.Location = new System.Drawing.Point(0, 0);
-            this.videoView1.MediaPlayer = null;
-            this.videoView1.Name = "videoView1";
-            this.videoView1.Size = new System.Drawing.Size(527, 714);
-            this.videoView1.TabIndex = 0;
-            this.videoView1.Text = "videoView1";
+            this.videoView.BackColor = System.Drawing.Color.Black;
+            this.videoView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoView.Location = new System.Drawing.Point(0, 0);
+            this.videoView.MediaPlayer = null;
+            this.videoView.Name = "videoView";
+            this.videoView.Size = new System.Drawing.Size(527, 714);
+            this.videoView.TabIndex = 0;
+            this.videoView.Text = "videoView";
             // 
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(5, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(265, 54);
+            this.button1.Size = new System.Drawing.Size(255, 54);
             this.button1.TabIndex = 2;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button_Click);
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1});
+            this.toolStripStatusLabel});
             this.statusStrip1.Location = new System.Drawing.Point(0, 692);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(796, 22);
             this.statusStrip1.TabIndex = 3;
             this.statusStrip1.Text = "statusStrip1";
             // 
-            // toolStripStatusLabel1
+            // toolStripStatusLabel
             // 
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
-            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
             // 
             // splitContainer1
             // 
@@ -87,30 +87,42 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox);
             this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(5);
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.videoView1);
+            this.splitContainer1.Panel2.Controls.Add(this.videoView);
             this.splitContainer1.Size = new System.Drawing.Size(796, 714);
             this.splitContainer1.SplitterDistance = 265;
             this.splitContainer1.TabIndex = 4;
             // 
-            // Form1
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox.Location = new System.Drawing.Point(5, 59);
+            this.textBox.Margin = new System.Windows.Forms.Padding(30);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(255, 20);
+            this.textBox.TabIndex = 3;
+            // 
+            // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(796, 714);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.splitContainer1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            this.Name = "Form";
+            this.Text = "TakeWebCamSnap";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_FormClosing);
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.videoView)).EndInit();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
@@ -121,11 +133,12 @@
 
         #endregion
 
-        private LibVLCSharp.WinForms.VideoView videoView1;
+        private LibVLCSharp.WinForms.VideoView videoView;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
         private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox;
     }
 }
 
---
+++ TakeWebCamSnapshot/Form.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibVLCSharp.Shared;
+
+namespace TakeWebCamSnapshot
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private MediaPlayer CreateMediaPlayer()
+        {
+            var _libVLC = new LibVLC();
+
+            _libVLC.SetDialogHandlers( ( _, _ ) => Task.CompletedTask,
+                ( _, _, _, _, _, _ ) => Task.CompletedTask,
+                ( _, _, _, _, _, _, _, _ ) => Task.CompletedTask,
+                ( _, _, _, _, _, _, _ ) => Task.CompletedTask,
+                ( _, _, _ ) => Task.CompletedTask );
+
+            var media = new Media( _libVLC, "dshow//:", FromType.FromLocation );
+            media.AddOption( ":live-caching=0" );
+            media.AddOption( ":no-audio" );
+            media.AddOption( ":dshow-fps=30" );
+            media.AddOption( ":dshow-adev=none" );
+            media.AddOption( ":no-dshow-config" );
+            media.AddOption( ":no-dshow-tuner" );
+            var _mp = new MediaPlayer( _libVLC )
+            {
+                Media = media
+            };
+
+            if ( !_mp.Play() ) toolStripStatusLabel.Text = "Error in Playing...";
+            return _mp;
+        }
+
+        private void TakeSnapshot()
+        {
+            if ( !videoView.MediaPlayer?.TakeSnapshot( 0, textBox.Text,
+                    0, 0 ) ?? true )
+                toolStripStatusLabel.Text = "Fail Take Snapshot";
+        }
+
+        private void Form_Load( object sender, EventArgs e )
+        {
+            videoView.MediaPlayer = CreateMediaPlayer();
+            textBox.Text = Path.Combine( Path.GetDirectoryName( Assembly.GetEntryAssembly().Location ),
+                "current_snap.png" );
+        }
+
+        private void button_Click( object sender, EventArgs e )
+        {
+            TakeSnapshot();
+        }
+
+        private void Form_FormClosing( object sender, FormClosingEventArgs e )
+        {
+            videoView.MediaPlayer?.Dispose();
+        }
+    }
+}
