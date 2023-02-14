b842a68584600c6aef9b526078ce28921639805a
Semyon Labzov <s-labzov@armo.ru>
2023-02-14 09:02:46 +0300

    - initial commit

---
+++ TakeWebCamSnapshot/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TakeWebCamSnapshot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // videoView1
+            // 
+            this.videoView1.BackColor = System.Drawing.Color.Black;
+            this.videoView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoView1.Location = new System.Drawing.Point(0, 0);
+            this.videoView1.MediaPlayer = null;
+            this.videoView1.Name = "videoView1";
+            this.videoView1.Size = new System.Drawing.Size(527, 714);
+            this.videoView1.TabIndex = 0;
+            this.videoView1.Text = "videoView1";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 692);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(796, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.videoView1);
+            this.splitContainer1.Size = new System.Drawing.Size(796, 714);
+            this.splitContainer1.SplitterDistance = 265;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 714);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView videoView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ TakeWebCamSnapshot/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibVLCSharp.Shared;
+
+namespace TakeWebCamSnapshot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private MediaPlayer _mp;
+
+
+        private void Form1_Load( object sender, EventArgs e )
+        {
+            var _libVLC = new LibVLC();
+
+            _libVLC.SetDialogHandlers( ( _, _ ) => Task.CompletedTask,
+                ( dialog, _, _, _, _, _ ) =>
+                {
+                    //dialog.PostLogin(Settings.Login, Settings.Password, false);
+                    return Task.CompletedTask;
+                },
+                ( _, _, _, _, _, _, _, _ ) => Task.CompletedTask,
+                ( _, _, _, _, _, _, _ ) => Task.CompletedTask,
+                ( _, _, _ ) => Task.CompletedTask );
+
+            var media = new Media( _libVLC, "dshow//:", FromType.FromLocation );
+            media.AddOption( ":live-caching=0" );
+            media.AddOption( ":no-audio" );
+            media.AddOption( ":dshow-fps=30" );
+            media.AddOption( ":dshow-adev=none" );
+            media.AddOption( ":no-dshow-config" );
+            media.AddOption( ":no-dshow-tuner" );
+            _mp = new MediaPlayer( _libVLC )
+            {
+                Media = media
+            };
+
+            if ( !_mp.Play() ) toolStripStatusLabel1.Text = "Error in Playing...";
+
+            videoView1.MediaPlayer = _mp;
+        }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+            if ( !_mp.TakeSnapshot( 0,
+                    "D:\\Projects\\TakeWebCamSnapshot\\TakeWebCamSnapshot\\bin\\Debug\\current_snap.png",
+                    0, 0 ) )
+                toolStripStatusLabel1.Text = "Fail TakeSnapshot";
+        }
+
+        private void Form1_FormClosing( object sender, FormClosingEventArgs e )
+        {
+            _mp.Dispose();
+        }
+    }
+}
